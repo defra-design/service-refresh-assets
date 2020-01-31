567c15b884ab0f8c5107dcae34af9354050eb28d
Eleanor Shakeshaft <eleanor.shakeshaft@defra.gov.uk>
2020-01-31 11:25:28 +0000

    Test asset files

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,8 @@ const router = express.Router()
 router.get('/fis-454-draft', function (req, res) {
   res.render('assets/fis-454-draft')
 })
+router.get('/fis-123-draft', function (req, res) {
+  res.render('assets/fis-123-draft')
+})
 
 module.exports = router
