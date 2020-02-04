795c410c10482e5316beffb72780b0ff4509d1d4
danleech-defra <danleech-defra@gmail.com>
2020-02-04 10:42:58 +0000

    Map link added to national view

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,9 @@ const router = express.Router()
 router.get('/fis-123-draft', function (req, res) {
   res.render('assets/fis-123-draft')
 })
+router.get('/fis-155-draft', function (req, res) {
+  res.render('assets/fis-155-draft')
+})
 router.get('/fis-256-draft', function (req, res) {
   res.render('assets/fis-256-draft')
 })
