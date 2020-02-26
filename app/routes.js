2c6b371dfc90a9eb00175a994b8be408d0a8221e
danleech-defra <danleech-defra@gmail.com>
2020-02-26 09:56:58 +0000

    Assest for station page start

--- app/routes.js
+++ app/routes.js
@@ -14,6 +14,9 @@ router.get('/fis-256-draft', function (req, res) {
 router.get('/fis-260-draft', function (req, res) {
   res.render('assets/fis-260-draft')
 })
+router.get('/fis-366-draft', function (req, res) {
+  res.render('assets/fis-366-draft')
+})
 router.get('/fis-375-draft', function (req, res) {
   res.render('assets/fis-375-draft')
 })
