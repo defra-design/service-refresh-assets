9499572664e2a125310fde9af162417ed4a49d2c
danleech-defra <danleech-defra@gmail.com>
2020-03-13 13:10:42 +0000

    v1 created

--- app/routes.js
+++ app/routes.js
@@ -14,17 +14,17 @@ router.get('/fis-256-draft', function (req, res) {
 router.get('/fis-260-draft', function (req, res) {
   res.render('assets/fis-260-draft')
 })
-router.get('/fis-366-page-1-draft', function (req, res) {
-  res.render('assets/fis-366-page-1-draft')
+router.get('/fis-366-page-1-v1', function (req, res) {
+  res.render('assets/fis-366-page-1-v1')
 })
-router.get('/fis-366-page-2-draft', function (req, res) {
-  res.render('assets/fis-366-page-2-draft')
+router.get('/fis-366-page-2-v1', function (req, res) {
+  res.render('assets/fis-366-page-2-v1')
 })
-router.get('/fis-366-page-3-draft', function (req, res) {
-  res.render('assets/fis-366-page-3-draft')
+router.get('/fis-366-page-3-v1', function (req, res) {
+  res.render('assets/fis-366-page-3-v1')
 })
-router.get('/fis-366-errors-draft', function (req, res) {
-  res.render('assets/fis-366-errors-draft')
+router.get('/fis-366-page-4-v1', function (req, res) {
+  res.render('assets/fis-366-page-4-v1')
 })
 router.get('/fis-375-draft', function (req, res) {
   res.render('assets/fis-375-draft')
