f62fb8bf9fb00281fbea5e379872477470cf328e
Eleanor Shakeshaft <eleanor.shakeshaft@defra.gov.uk>
2020-06-05 16:12:34 +0100

    content changes

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 // Add your assets here
-router.get('/fis-start-page-ideas', function (req, res) {
-  res.render('assets/fis-start-page-ideas')
+router.get('/fis-start-page', function (req, res) {
+  res.render('assets/fis-start-page')
 })
 router.get('/fis-152-draft', function (req, res) {
   res.render('assets/fis-152-draft')
