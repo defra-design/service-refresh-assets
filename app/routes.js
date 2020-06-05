789988e0e64f598aee81c69fcb72f9e457962f0d
Eleanor Shakeshaft <eleanor.shakeshaft@defra.gov.uk>
2020-06-05 16:15:45 +0100

    changed fis-1 to asset-1

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Add your assets here
-router.get('/fis-1', function (req, res) {
+router.get('/asset-1', function (req, res) {
   res.render('assets/fis-1')
 })
 router.get('/fis-152-draft', function (req, res) {
