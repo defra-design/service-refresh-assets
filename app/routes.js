39823ec3edf057adb6d6beff26042d2c6a9eb16b
danleech-defra <danleech-defra@gmail.com>
2020-10-19 15:54:06 +0100

    Cookies page

--- app/routes.js
+++ app/routes.js
@@ -110,6 +110,9 @@ router.get('/asset-16b', function (req, res) {
 router.get('/asset-17', function (req, res) {
   res.render('assets/asset-17')
 })
+router.get('/asset-18', function (req, res) {
+  res.render('assets/asset-18')
+})
 router.get('/icons', function (req, res) {
   res.render('assets/icons')
 })
