e8c743347e9d9431e941696c67beb3d14fd14ea4
danleech-defra <danleech-defra@gmail.com>
2020-10-21 16:27:54 +0100

    Reserved var name

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -1,29 +1,29 @@
 var container = document.getElementById('cookie-accept')
 
 if (container) {
-  var accept = document.createElement('button')
-  accept.className = 'defra-cookie-banner__button-accept'
-  accept.innerText = 'Accept all cookies'
-  container.parentNode.replaceChild(accept, container)
+  var acceptButton = document.createElement('button')
+  acceptButton.className = 'defra-cookie-banner__button-accept'
+  acceptButton.innerText = 'Accept all cookies'
+  container.parentNode.replaceChild(acceptButton, container)
 
-  accept.addEventListener('click', (e) => {
+  acceptButton.addEventListener('click', (e) => {
     e.preventDefault()
     document.getElementById('cookie-message').style.display = 'none'
     document.getElementById('cookie-confirmation').removeAttribute('style')
   })
 
-  var hide = document.getElementById('cookie-hide')
+  var hideButton = document.getElementById('cookie-hide')
 
-  hide.addEventListener('click', (e) => {
+  hideButton.addEventListener('click', (e) => {
     e.preventDefault()
     document.getElementById('cookie-banner').style.display = 'none'
   })
 }
 
-var save = document.getElementById('cookies-save')
+var saveButton = document.getElementById('cookies-save')
 
-if (save) {
-  save.addEventListener('click', (e) => {
+if (saveButton) {
+  saveButton.addEventListener('click', (e) => {
     e.preventDefault()
     var alert = document.getElementById('cookie-save-confirmation')
     alert.removeAttribute('style')
