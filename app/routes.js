d216a891485df523ae2c13b036809b5ffe387521
danleech-defra <danleech-defra@gmail.com>
2020-11-04 09:57:50 +0000

    Bing assets

--- app/routes.js
+++ app/routes.js
@@ -116,6 +116,15 @@ router.get('/asset-18', function (req, res) {
 router.get('/asset-19', function (req, res) {
   res.render('assets/asset-19')
 })
+router.get('/asset-20', function (req, res) {
+  res.render('assets/asset-20')
+})
+router.get('/asset-20b', function (req, res) {
+  res.render('assets/asset-20b')
+})
+router.get('/asset-20c', function (req, res) {
+  res.render('assets/asset-20c')
+})
 router.get('/icons', function (req, res) {
   res.render('assets/icons')
 })
