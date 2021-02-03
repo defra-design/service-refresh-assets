a15c3e8aac4817b821a0aaac4edb3c86d103eddb
danleech-defra <danleech-defra@gmail.com>
2021-02-03 16:32:10 +0000

    Stuff

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -2,8 +2,23 @@ window.flood = {}
 
 // Filter list
 window.flood.Filter = (id) => {
-  const facetsContainer = document.getElementById(id).firstElementChild
-  console.log(facetsContainer)
+  const facetsContainer = document.getElementById(id).querySelector('.defra-facets__container')
+  const showFiltersButton = document.createElement('button')
+  showFiltersButton.className = 'defra-facets__show-filters'
+  showFiltersButton.innerHTML = 'Filters'
+  const filtersCount = document.createElement('span')
+  filtersCount.innerHTML = ' (1)'
+  showFiltersButton.appendChild(filtersCount)
+  facetsContainer.appendChild(showFiltersButton)
+  facetsContainer.parentNode.insertBefore(showFiltersButton, facetsContainer.parentNode.firstChild)
+
+  //
+  // Events
+  //
+
+  showFiltersButton.addEventListener('click', () => {
+    facetsContainer.setAttribute('open', true)
+  })
 
   // Mobile behavior
   const mobileMediaQuery = window.matchMedia('(max-width: 640px)')
