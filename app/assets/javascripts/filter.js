278d7ec7c390190605c14d18752e11f624074c2d
danleech-defra <danleech-defra@gmail.com>
2021-02-03 17:44:49 +0000

    Stuff

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -2,34 +2,55 @@ window.flood = {}
 
 // Filter list
 window.flood.Filter = (id) => {
-  const facetsContainer = document.getElementById(id).querySelector('.defra-facets__container')
-  const showFiltersButton = document.createElement('button')
-  showFiltersButton.className = 'defra-facets__show-filters'
-  showFiltersButton.innerHTML = 'Filters'
+  const container = document.getElementById(id).querySelector('.defra-facets__container')
+  const inner = container.querySelector('.defra-facets__inner')
+  const showFilters = document.createElement('button')
+  showFilters.className = 'defra-facets__show-filters'
+  showFilters.innerHTML = 'Filters'
   const filtersCount = document.createElement('span')
   filtersCount.innerHTML = ' (1)'
-  showFiltersButton.appendChild(filtersCount)
-  facetsContainer.appendChild(showFiltersButton)
-  facetsContainer.parentNode.insertBefore(showFiltersButton, facetsContainer.parentNode.firstChild)
+  showFilters.appendChild(filtersCount)
+  const closeFilters = document.createElement('button')
+  closeFilters.className = 'defra-facets__close-filters'
+  closeFilters.innerHTML = 'Return to results'
+  const clearFilters = document.createElement('button')
+  clearFilters.className = 'defra-facets__clear-filters'
+  clearFilters.innerHTML = 'Clear filters'
+  const filterResults = container.querySelector('.defra-facets__filter-results')
+  inner.insertBefore(closeFilters, inner.firstChild)
+  inner.appendChild(clearFilters)
+  container.appendChild(showFilters)
+  container.parentNode.insertBefore(showFilters, container.parentNode.firstChild)
 
   //
   // Events
   //
 
-  showFiltersButton.addEventListener('click', () => {
-    facetsContainer.setAttribute('open', true)
-  })
-
-  // Mobile behavior
+  // Detect mobile/non-mobile
   const mobileMediaQuery = window.matchMedia('(max-width: 640px)')
   const mobileListener = (mobileMediaQuery) => {
     const isMobile = mobileMediaQuery.matches
-    isMobile ? facetsContainer.setAttribute('aria-modal', 'true') : facetsContainer.removeAttribute('aria-modal')
-    isMobile ? facetsContainer.setAttribute('tabindex', 0) : facetsContainer.removeAttribute('tabindex')
+    isMobile ? container.setAttribute('aria-modal', 'true') : container.removeAttribute('aria-modal')
+    isMobile ? container.setAttribute('tabindex', 0) : container.removeAttribute('tabindex')
     console.log('isMobile: ' + isMobile)
   }
   mobileMediaQuery.addListener(mobileListener)
   mobileListener(mobileMediaQuery)
+
+  // Show filters (mobile only)
+  showFilters.addEventListener('click', () => {
+    container.setAttribute('open', true)
+  })
+
+  // Close filters (mobile only)
+  closeFilters.addEventListener('click', () => {
+    container.setAttribute('open', false)
+  })
+
+  // Filter results (asynchronous)
+  filterResults.addEventListener('click', () => {
+    container.setAttribute('open', false)
+  })
 }
 
 // Create filter
