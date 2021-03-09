442fdfb03eaaa21639c380737824a4e9f7b3b2a4
danleech-defra <danleech-defra@gmail.com>
2021-03-09 14:31:32 +0000

    Stuff

--- gulp/webpack.js
+++ gulp/webpack.js
@@ -10,26 +10,38 @@ const named = require('vinyl-named')
 const config = require('./config.json')
 const webpackStream = require('webpack-stream')
 
+// gulp.task('webpack', function (done) {
+//   gulp.src([
+//     config.paths.src + 'js/core.js',
+//     config.paths.src + 'js/pages/station.js',
+//     config.paths.src + 'js/pages/river-and-sea-levels.js'
+//   ])
+//     .pipe(named())
+//     .pipe(webpackStream({
+//       mode: 'production', // development, production
+//       devtool: 'none', // source-map, none
+//       // output: {
+//       //   path: path.resolve(__dirname, config.paths.dist + 'js/')
+//       // },
+//       module: {
+//         rules: [
+//           {
+//             use: {
+//               loader: 'babel-loader'
+//             }
+//           }
+//         ]
+//       }
+//     }))
+//     .pipe(gulp.dest(config.paths.dist + 'js/'))
+//   done()
+// })
+
 gulp.task('webpack', function (done) {
   gulp.src([
-    config.paths.src + 'js/core.js',
-    config.paths.src + 'js/pages/station.js',
-    config.paths.src + 'js/pages/river-and-sea-levels.js'
-  ])
+    config.paths.src + 'js/core.js'])
     .pipe(named())
-    .pipe(webpackStream({
-      mode: 'production', // development, production
-      devtool: 'none', // source-map, none
-      module: {
-        rules: [
-          {
-            use: {
-              loader: 'babel-loader'
-            }
-          }
-        ]
-      }
-    }))
+    .pipe(webpackStream())
     .pipe(gulp.dest(config.paths.dist + 'js/'))
   done()
 })
