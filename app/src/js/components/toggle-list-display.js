fcf2d5385d6f73a937a94a8832b832ba9eb30706
danleech-defra <danleech-defra@gmail.com>
2021-05-12 09:35:23 +0100

    aria-expanded fixed

--- app/src/js/components/toggle-list-display.js
+++ app/src/js/components/toggle-list-display.js
@@ -4,28 +4,36 @@
 const { forEach } = window.flood.utils
 
 const ToggleListDisplay = (container, options) => {
+  let isExpanded = false
   const list = document.querySelector('.defra-flood-impact-list')
   const items = list.querySelectorAll(`[data-toggle-list-display-item="${options.type}"]`)
   const button = document.createElement('button')
   button.className = 'defra-button-text govuk-!-margin-bottom-2'
-  button.innerText = `Show ${options.btnText}`
-  button.setAttribute('aria-expanded', false)
   button.setAttribute('aria-controls', list.id)
   container.appendChild(button)
 
   const toggleDisplay = () => {
+    // Toggle Button
+    button.innerText = `${isExpanded ? 'Hide' : 'Show'} ${options.btnText}`
+    button.setAttribute('aria-expanded', isExpanded)
+    // Toggle list
     forEach(items, (item) => {
-      item.style.display = item.style.display === 'block' ? 'none' : 'block'
+      item.style.display = isExpanded ? 'block' : 'none'
     })
   }
+
+  //
+  // Initialise
+  //
+
+  toggleDisplay()
 
   //
   // Events
   //
 
   button.addEventListener('click', (e) => {
-    button.innerText = `${button.innerText.startsWith('Show') ? 'Hide' : 'Show'} ${options.btnText}`
-    button.setAttribute('aria-expanded', button.getAttribute('aria-expanded') === 'true' ? false : true)
+    isExpanded = !isExpanded
     toggleDisplay()
   })
 }
