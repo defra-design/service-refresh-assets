66131decdf2e686cdbf3ecaf8c04bafac4e04053
danleech-defra <danleech-defra@gmail.com>
2021-06-08 10:26:15 +0100

    Webpack config change

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const env = process.env.NODE_ENV
-// const inDev = env === 'dev' || env === 'development'
-const inDev = true
+const inDev = env === 'dev' || env === 'development'
 
 module.exports = (env, argv) => ({
   mode: !inDev ? 'production' : 'development',
@@ -22,7 +21,7 @@ module.exports = (env, argv) => ({
     rules: [
       {
         // Default exclude removes all node_modules but d3 is now distributed es6 so include d3 (& our own src) in transpile
-        include: mPath => mPath.indexOf('server/src') > -1 || mPath.indexOf('node_modules/d3') > -1,
+        include: mPath => mPath.indexOf('app/src') > -1 || mPath.indexOf('node_modules/d3') > -1,
         use: {
           loader: 'babel-loader',
           options: {
